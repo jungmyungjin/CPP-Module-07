90f210adf66bed61db844a49c7b422a69c6365ab
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2021-12-22 23:17:55 +0900

    ex00 템플릿 두개 추가

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,25 +6,25 @@
 /*   By: mjung <mjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 22:56:19 by mjung             #+#    #+#             */
-/*   Updated: 2021/12/22 23:00:13 by mjung            ###   ########.fr       */
+/*   Updated: 2021/12/22 23:14:58 by mjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "whatever.hpp"
 
-int main(void)
-{
-	int n1 = 10, n2 = 20;
-	double d1 = 1.1, d2 = 2.2;
-
-	std::cout << n1 << ", " << n2 << std::endl;
-	Swap(n1, n2);
-	std::cout << n1 << ", " << n2 << std::endl;
-
-	std::cout << d1 << ", " << d2 << std::endl;
-	Swap(d1, d2);
-	std::cout << d1 << ", " << d2 << std::endl;
-
-	return (0);
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: mjung <mjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 22:56:30 by mjung             #+#    #+#             */
-/*   Updated: 2021/12/22 23:00:57 by mjung            ###   ########.fr       */
+/*   Updated: 2021/12/22 23:17:15 by mjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,29 @@
 #define WHATEVER_H
 
 template <typename T>
-void Swap(T &a, T &b)
+void swap(T &a, T &b)
 {
 	T tmp = a;
 	a = b;
 	b = tmp;
+};
+
+template <typename T>
+T min(T &a, T &b)
+{
+	if (a < b)
+		return a;
+	else 
+		return b;
+};
+template <typename T>
+
+T max(T &a, T &b)
+{
+	if (a > b)
+		return a;
+	else 
+		return b;
 };
 
 #endif
